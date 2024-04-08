0247c1b8e55d3028d7cbb4a93c4f3c07686cb647
Megoadro <sayedkhalifa086@gmail.com>
2024-04-08 17:44:33 +0200

    hive init at main

--- lib/helper/contstan.dart
+++ lib/helper/contstan.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = const Color(0xffFFBF00);
+const kNotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/helper/contstan.dart';
 import 'package:note_app/views/home_view.dart';
 
-void main (){
-
+void main () async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(NoteApp());
 }
 class NoteApp extends StatelessWidget {
