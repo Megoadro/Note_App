ae4f0c7477284d9aaa6683aa815c111e683995d1
Megoadro <sayedkhalifa086@gmail.com>
2024-04-08 19:07:05 +0200

    create Method For add Note And rigsterHive atMain

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/helper/contstan.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/home_view.dart';
 
 void main () async{
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(NoteApp());
 }
 class NoteApp extends StatelessWidget {
