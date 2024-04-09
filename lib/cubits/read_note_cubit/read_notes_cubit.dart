fe2cb7710daea9971b7e822a575b0bc8e4b4220c
Megoadro <sayedkhalifa086@gmail.com>
2024-04-09 22:11:51 +0200

    add BlocProvider To HomeView

--- lib/cubits/read_note_cubit/read_notes_cubit.dart
+++ lib/cubits/read_note_cubit/read_notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/helper/contstan.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'read_notes_state.dart';
 
 class ReadNotesCubit extends Cubit<ReadNotesState> {
   ReadNotesCubit() : super(ReadNotesInitial());
+
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(ReadNotesSuccess(notes: notes));
+    } catch (e) {
+      emit(ReadNotesFailuer(e.toString()));
+    }
+  }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/read_note_cubit/read_notes_cubit.dart';
 import 'package:note_app/widgets/custom_body.dart';
 import 'package:note_app/widgets/modal_bottom_sheet.dart';
 
@@ -7,26 +9,29 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              context: context,
-              builder: (context) {
-                return const NoteModalBottomSheet();
-              });
-        },
-        backgroundColor: Colors.white,
-        child: const Icon(
-          Icons.add,
-          color: Colors.amber,
+    return BlocProvider(
+      create: (context) => ReadNotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                context: context,
+                builder: (context) {
+                  return const NoteModalBottomSheet();
+                });
+          },
+          backgroundColor: Colors.white,
+          child: const Icon(
+            Icons.add,
+            color: Colors.amber,
+          ),
         ),
+        body: const CustomBody(),
       ),
-      body: const CustomBody(),
     );
   }
 }
