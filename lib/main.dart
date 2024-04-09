010bea77de10eda7b8df46b5f874f447829338db
Megoadro <sayedkhalifa086@gmail.com>
2024-04-09 19:14:22 +0200

    Create simbleBlocObserver to main too

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/cubits/add_note_cubit/cubit/add_note_cubit.dart';
 import 'package:note_app/helper/contstan.dart';
 import 'package:note_app/models/note_model.dart';
+import 'package:note_app/simble_block_observer.dart';
 import 'package:note_app/views/home_view.dart';
 
 void main () async{
+  Bloc.observer=SimbleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
