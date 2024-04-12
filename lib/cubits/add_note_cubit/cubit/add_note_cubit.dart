123d4f92d9904dde51d8bb9646dd3d0d2e53ecd9
Megoadro <sayedkhalifa086@gmail.com>
2024-04-12 20:06:39 +0200

    modalBottomSheet Done

--- lib/cubits/add_note_cubit/cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/cubit/add_note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:note_app/helper/contstan.dart';
@@ -8,13 +9,15 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  Color color = const Color(0xff541388);
 
   addNote(NoteModel note) async {
+    note.color=color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
       await notesBox.add(note);
-     emit (AddNoteSuccess());
+      emit(AddNoteSuccess());
     } catch (e) {
       emit(AddNoteFailur(errMessege: e.toString()));
     }
